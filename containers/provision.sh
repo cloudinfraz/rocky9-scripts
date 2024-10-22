89f61620469b9f5380b03c2387df3cf705b9f1ae
Jimmy Zhang <zhang.arthur@gmail.com>
2024-10-22 21:41:02 +0800

    Update provision.sh

--- containers/provision.sh
+++ containers/provision.sh
@@ -12,9 +12,8 @@ if [ -f /opt/azure/containers/provision.complete ]; then
       echo "Already ran to success exiting..."
       exit 0
 fi
-cp -r /var/lib/waagent/Microsoft.Azure.Extensions.CustomScript-2.1.1 /root/Microsoft.Azure.Extensions.CustomScript-2.1.1
-
-cp -r /var/lib/waagent/Microsoft.AKS.Compute.AKS.Linux.AKSNode-1.154  /root/Microsoft.AKS.Compute.AKS.Linux.AKSNode-1.154
+#cp -r /var/lib/waagent/Microsoft.Azure.Extensions.CustomScript-2.1.1 /root/Microsoft.Azure.Extensions.CustomScript-2.1.1
+#cp -r /var/lib/waagent/Microsoft.AKS.Compute.AKS.Linux.AKSNode-1.154  /root/Microsoft.AKS.Compute.AKS.Linux.AKSNode-1.154
 
 aptmarkWALinuxAgent hold &
 
